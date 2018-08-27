08ae3baca07c07b2666ad9347b3bfe6868060a5a
IGrock88 <igrock88@gmail.com>
2018-08-27 19:42:53 +0800

    homework2

--- frontend/tests/functional/MyFirstCest.php
+++ frontend/tests/functional/MyFirstCest.php
@@ -4,6 +4,7 @@ use frontend\tests\FunctionalTester;
 
 class MyFirstCest
 {
+
     public function _before(FunctionalTester $I)
     {
     }
@@ -12,9 +13,28 @@ class MyFirstCest
     {
     }
 
-    // tests
-    public function tryToTest(FunctionalTester $I)
+    /**
+     * @dataProvider pageProvider
+     */
+    public function checkTextInH1Tags(FunctionalTester $I, \Codeception\Example $example)
     {
+        $I->amOnPage($example['url']);
+        $I->see($example['text'], 'h1');
 
+    }
+
+    /**
+     * @return array
+     */
+    protected function pageProvider()
+    {
+        return [
+            ['url'=>"/site/index", 'text'=>"Congratulations!"],
+            ['url'=>"/site/about", 'text'=>"About"],
+            ['url'=>"/site/contact", 'text'=>"Contact"],
+            ['url'=>"/site/signup", 'text'=>"Signup"],
+            ['url'=>"/site/login", 'text'=>"Login"],
+            ['url'=>"/hello/index", 'text'=>"Hello, World!"]
+        ];
     }
 }
