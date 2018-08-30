c79ac501193a5db22f2e0a6d1dffe1bd09c497d4
IGrock88 <igrock88@gmail.com>
2018-08-30 18:20:40 +0800

    fix

--- common/config/main.php
+++ common/config/main.php
@@ -6,8 +6,8 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'chat' => [
-            'class' => 'common\modules\chat\Module',
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
         ],
     ],
     'modules' => [
